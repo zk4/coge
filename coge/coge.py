764646b9c5d12ca33acca0e9b4980a2e3a4660fe
zk <liuzq7@gmail.com>
2020-08-12 16:07:16 +0800

    update

--- coge/coge.py
+++ coge/coge.py
@@ -191,16 +191,16 @@ def link():
     subprocess.check_output(f"ln  -s $PWD {root}", shell=True)
 
 def createParse():
-    parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="""
-      make template link : cd x-engine-module-template && coge -r
+    parser = argparse.ArgumentParser( formatter_class=argparse.RawTextHelpFormatter, description="""
+      make template link : cd x-engine-module-template && coge -r 
             use template : coge x-engine-module-template xxxx:camera @:x-engine-module-camera  
     """)
 
     parser.add_argument('-a', '--arg_prefix',type=str,required=False, help='ex: COGE_ARG__', default="COGE_ARG__")  
     parser.add_argument('-l', '--list', help='list folders', default=False, action='store_true' ,) 
-    parser.add_argument('-r', '--link_tplt', help='link cwd template to COGE_TMPLS', default=False, action='store_true' ) 
-    parser.add_argument('-i', '--init', help='init your COGE_TMPLS location to ~/.config/.code_template', default=False, action='store_true' ) 
-    parser.add_argument('-w', '--allow_git_dirty', help='by default, your COGE_TMPLS must git clean, because coge relies on git command if you are in a git repo', default=False, action='store_true' ) 
+    parser.add_argument('-r', '--link_tplt', help='link `cwd` to $COGE_TMPLS', default=False, action='store_true' ) 
+    # parser.add_argument('-i', '--init', help='init your COGE_TMPLS location to ~/.config/.code_template', default=False, action='store_true' ) 
+    parser.add_argument('-w', '--allow_git_dirty', help='alllow git dirty', default=False, action='store_true' ) 
     parser.add_argument('-d', '--depth',type=int,required=False, help='list depth', default=3)  
     parser.add_argument('magic', metavar="magic", type=str, nargs='*', 
             help='folder or newkey:oldkey')
